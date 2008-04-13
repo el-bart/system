76c3126dcb6e2e58680809830fb38db459fced88
el-bart <el-bart@tweety>
2008-04-13 16:04:21 +0200

    AutoCptr is wrapper for C-stype pointers;
    added:
      system/System/AutoCptr.hpp
      system/System/AutoCptr.t.cpp
    modified:
      RELEASE_NOTES
      TODO

---
+++ system/System/AutoCptr.hpp
@@ -0,0 +1,87 @@
+/*
+ * AutoCptr.hpp
+ *
+ * class behaving like an auto_ptr<> for
+ * C-allocated pointer.
+ *
+ */
+#ifndef INCLUDE_SYSTEM_AUTOCPTR_HPP_FILE
+#define INCLUDE_SYSTEM_AUTOCPTR_HPP_FILE
+
+/* public header */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <boost/operators.hpp>
+
+#include "System/AutoVariable.hpp"
+
+namespace System
+{
+
+/** \brief helper class for internal usage in AutoCptr.
+ */
+template<typename T>
+class CptrHolder
+{
+public:
+  /** \brief typedef for value type being held.
+   */
+  typedef T* TValue;
+  /** \brief constructor setting pointer value to be held.
+   *  \param v pointer to be held.
+   */
+  explicit CptrHolder(TValue v=NULL):
+    _v(v)
+  {
+  }
+  /** \brief returns pointer.
+   *  \return pointer being held.
+   */
+  inline TValue get(void) const
+  {
+    return _v;
+  }
+  /** \brief frees pointer held inside.
+   */
+  void dealocate(void)
+  {
+    if(_v!=NULL)
+    {
+      free(_v);
+      _v=NULL;
+    }
+  }
+
+private:
+  TValue _v;
+}; // class CptrHolder
+
+
+/** \brief auto_ptr<>-like class for holding C-allocated pointers.
+ */
+template<typename T>
+class AutoCptr: public boost::equality_comparable< AutoCptr<T> >,
+                public System::AutoVariable< CptrHolder<T> >
+{
+public:
+  /** \brief holder class.
+   */
+  typedef CptrHolder<T>            THolder;
+  /** \brief pointer type held inside.
+   */
+  typedef typename THolder::TValue TPtr;
+
+  /** \brief create C-pointer holder.
+   *  \param fs pointer to be held.
+   */
+  explicit AutoCptr(TPtr fs=NULL):
+    System::AutoVariable<THolder>(fs)
+  {
+  }
+}; // class AutoCptr
+
+} // namespace System
+
+#endif
+
---
+++ system/System/AutoCptr.t.cpp
@@ -0,0 +1,95 @@
+/*
+ * AutoCptr.t.cpp
+ *
+ */
+#include <tut.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "System/AutoCptr.hpp"
+
+namespace System
+{
+
+struct AutoCptrTestData
+{
+  typedef AutoCptr<void> ACP;
+
+  AutoCptrTestData(void):
+    _ap1( malloc(2)      ),
+    _ap2( malloc(4*1024) )
+  {
+    tut::ensure( _ap1.get()!=NULL );
+    tut::ensure( _ap2.get()!=NULL );
+  }
+
+  ACP _ap1;
+  ACP _ap2;
+};
+
+} // namespace System
+
+
+namespace tut
+{
+typedef System::AutoCptrTestData TestClass;
+typedef test_group<TestClass> factory;
+typedef factory::object testObj;
+} // namespace tut
+
+
+namespace
+{
+tut::factory tf("System/AutoCptr");
+}
+
+using namespace System;
+
+namespace tut
+{
+
+// check read/write from small memory pointer.
+template<>
+template<>
+void testObj::test<1>(void)
+{
+  char *p1=static_cast<char*>( _ap1.get() );
+  for(int i=0; i<2; ++i)
+  {
+    *(p1+i)='x';            // write.
+    ensure( *(p1+i)=='x' ); // stupid, but reads. ;)
+  }
+}
+
+// test r/w for biger memory part pointer.
+template<>
+template<>
+void testObj::test<2>(void)
+{
+  char *p2=static_cast<char*>( _ap2.get() );
+  for(int i=0; i<4*1024; ++i)
+  {
+    *(p2+i)='x';            // write.
+    ensure( *(p2+i)=='x' ); // stupid, but reads. ;)
+  }
+}
+
+// test for memory leaks by allocating a lot of memory
+template<>
+template<>
+void testObj::test<3>(void)
+{
+  const size_t size=100*1024*1024;   // 100MB
+  for(size_t i=0; i<10; ++i)
+  {
+    ACP p( malloc(size) );          // allocate and catch.
+    ensure( p.get()!=NULL );        // test if it passed.
+    memset( p.get(), 0, size );     // write something to memory.
+    // here goes release.
+  }
+}
+
+} // namespace tut
+
