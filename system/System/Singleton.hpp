8a4c83a7cd36377a088ed1698b062deb052783cd
el-bart <el-bart@tweety>
2008-12-08 17:04:35 +0100

    Singleton now uses AtExit to ensure proper creation/destruction order; test have been written;
    modified:
      system/System/Singleton.hpp
      system/System/Singleton.t.cpp

--- system/System/Singleton.hpp
+++ system/System/Singleton.hpp
@@ -24,6 +24,10 @@ namespace System
  * implementation is enhanced to allow secure usage, allowing
  * creating objects in proper order destroying in reverse order.
  *
+ * \noteit it is recommended to make constructor of class T
+ *         private and make Singleton<T> its friend. this ensures
+ *         that instances of T won't be made directly by user.
+ *
  */
 template<typename T>
 class Singleton: private boost::noncopyable
@@ -40,20 +44,58 @@ public:
    */
   inline static T *get(void)
   {
-    static T *t=init();
-    assert(t!=NULL);
+    static T *t=init(&t);
+    assert(t!=NULL &&
+           "object's initialization failed (not RAII-compliant?) or "
+           "using object after its deallocation");
     return t;
   }
 
 private:
-  static T *init(void)
+  // special deallocator type that ensures to NULL internal
+  // pointer as well.
+  class SingletonDeallocator: public AtExitResourceDeallocator
   {
-    std::auto_ptr<T> ap(new T);
-    assert( ap.get()!=NULL );
-    T *t=ap.get();
-    // TODO
+  public:
+    // create member when initialized
+    explicit SingletonDeallocator(T **dstPtr):
+      dstPtr_(dstPtr)
+    {
+      assert(dstPtr!=NULL);     // check input
+      *dstPtr_=new T;           // create pointer
+      assert(*dstPtr!=NULL);    // just in case... (RAII assumed)
+    }
+    // free when exiting
+    virtual ~SingletonDeallocator(void)
+    {
+      assert(*dstPtr_==NULL);   // object should be already freed
+      delete *dstPtr_;          // just in case...
+    }
+    // deallocation on exit
+    virtual void deallocate(void)
+    {
+      delete *dstPtr_;          // delete object
+      *dstPtr_=NULL;            // mark pointer as unusable
+    }
+  private:
+    T **dstPtr_;
+  }; // class SingletonDeallocator
+
+  // helper method that creates output object. this allows to keep
+  // singleton's get() as clean and simple as possible.
+  static T *init(T **dstPtr)
+  {
+    assert(dstPtr!=NULL);
+    // register singleton's deallocator
+    AtExit::TDeallocPtr dealloc( new SingletonDeallocator(dstPtr) );
+    assert( dealloc.get()!=NULL );
+    AtExit::registerDeallocator(dealloc);
+
+    // return pointer as a result
+    T *t=*dstPtr;
+    assert(t!=NULL);
     return t;
-  }
+  } // init()
 
   Singleton(void);  // no instances allowed
 }; // class Singleton
--- system/System/Singleton.t.cpp
+++ system/System/Singleton.t.cpp
@@ -8,25 +8,28 @@
 
 namespace
 {
-  struct MySingleType
+struct MySingleType
+{
+  void inc(void)
   {
-    MySingleType(void):
-      _cnt(0)
-    {
-    }
+    ++_cnt;
+  }
 
-    void inc(void)
-    {
-      ++_cnt;
-    }
+  int get(void) const
+  {
+    return _cnt;
+  }
 
-    int get(void) const
-    {
-      return _cnt;
-    }
-  private:
-    int _cnt;
-  }; // struct MySingleType
+private:
+  friend class System::Singleton<MySingleType>;
+
+  MySingleType(void):
+    _cnt(0)
+  {
+  }
+
+  int _cnt;
+}; // struct MySingleType
 } // unnamed namespace
 
 
@@ -55,9 +58,13 @@ tut::factory tf("System/Singleton");
 
 typedef System::Singleton<MySingleType> TestSingleton;
 
+using namespace System;
+
+
 namespace tut
 {
 
+// test get/increment
 template<>
 template<>
 void testObj::test<1>(void)
@@ -71,13 +78,89 @@ void testObj::test<1>(void)
   ensure( TestSingleton::get()->get()==2 );
 }
 
-
+// test if it is truely global
 template<>
 template<>
 void testObj::test<2>(void)
 {
   TestSingleton::get()->inc();
   ensure( TestSingleton::get()->get()==3 );
+}
+
+namespace
+{
+
+// table of destroyed OrderSingleton marks. each destroyed
+// singleton wil mark its absence with flag here
+bool tab[6]={false, false, false,
+             false, false, false};
+
+template<int N>
+struct OrderSingleton
+{
+  ~OrderSingleton(void)
+  {
+    for(int i=0; i<N; ++i)      // ceck desctruction order 1
+      assert(tab[i]==true);
+
+    tab[N]=true;                // mark self as destroyed
+
+    const int s=sizeof(tab)/sizeof(tab[0]);
+    for(int i=N+1; i<s; ++i)    // ceck desctruction order 2
+      assert(tab[i]==false);
+  }
+}; // struct MySingleType
+} // unnamed namespace
+
+// test if creation order reflects destruction order.
+// this is smoke test - if it won't crash at exit, it
+// means it works fine.
+template<>
+template<>
+void testObj::test<3>(void)
+{
+  Singleton< OrderSingleton<5> >::get();    // create
+  Singleton< OrderSingleton<4> >::get();    // create
+  Singleton< OrderSingleton<3> >::get();    // create
+  Singleton< OrderSingleton<2> >::get();    // create
+  Singleton< OrderSingleton<1> >::get();    // create
+  Singleton< OrderSingleton<0> >::get();    // create
+  // dealocation should that place in reverse order
+}
+
+namespace
+{
+struct SomeTestData
+{
+  int x;
+}; // struct SomeTestData
+typedef Singleton<SomeTestData> TestNullSingleton;
+
+struct TestExitNull: public AtExitResourceDeallocator
+{
+  virtual void deallocate(void)
+  {
+    // NOTE: test has been commented since Singleton checks
+    //       if pointer isn't NULL by default.
+    // pointer should be free and NULL'ed by now
+    //assert( TestNullSingleton::get()==NULL );
+  }
+}; // struct TestExitNull
+} // unnamed namespace
+
+// test if get after deallocation returns NULL. this
+// is a smoke test - if it doesn't crash at exit, it
+// means it works fine.
+template<>
+template<>
+void testObj::test<4>(void)
+{
+  // at first register checker
+  AtExit::TDeallocPtr dealloc(new TestExitNull);
+  AtExit::registerDeallocator(dealloc);
+  // now register singleton (implicit) and test if pointer is fine
+  ensure("object inside singleton not created",
+         TestNullSingleton::get()!=NULL);
 }
 
 } // namespace tut
