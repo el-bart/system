df688435c63bc5bf104d8b0e8d34c7e6c632fa88
el-bart <el-bart@klapex>
2009-07-21 09:48:41 +0200

    added new exception; c-tors are now explicit;
    added:
      system/System/ExceptionSyscallFailed.hpp
    modified:
      system/System/Exception.hpp

--- system/System/ExceptionSyscallFailed.hpp
+++ system/System/ExceptionSyscallFailed.hpp
@@ -1,34 +1,34 @@
 /*
- * Exception.hpp
+ * ExceptionSyscallFailed.hpp
  *
  * this is implementation class of base exception for System library and
  * should not be used as a base class outside of it.
  *
  */
-#ifndef INCLUDE_SYSTEM_EXCEPTION_HPP_FILE
-#define INCLUDE_SYSTEM_EXCEPTION_HPP_FILE
+#ifndef INCLUDE_SYSTEM_EXCEPTIONSYSCALLFAILED_HPP_FILE
+#define INCLUDE_SYSTEM_EXCEPTIONSYSCALLFAILED_HPP_FILE
 
 /* public header */
 
-#include "System/Exceptions/Base.hpp"
+#include "System/Exceptions/RuntimeError.hpp"
 
 namespace System
 {
 
 /** \brief internal exception class.
  */
-class Exception: public Exceptions::Base<Exception, std::exception>
+class ExceptionSyscallFailed: public Exceptions::RuntimeError
 {
 public:
   /** \brief create execption with given message.
    *  \param msg message to represent.
    */
   template<typename T>
-  explicit Exception(const T &msg):
-    Exceptions::Base<Exception, std::exception>(msg)
+  explicit ExceptionSyscallFailed(const T &msg):
+    RuntimeError(msg)
   {
   }
-}; // class Exception
+}; // class ExceptionSyscallFailed
 
 }; // namespace System
 
