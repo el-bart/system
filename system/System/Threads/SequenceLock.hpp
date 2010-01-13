5be948289595d5bb035bfccb43da3cda8751f9a4
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-01-13 17:18:21 +0100

    work-in-progress code.
    added:
      system/System/Threads/SequenceLock.hpp

---
+++ system/System/Threads/SequenceLock.hpp
@@ -0,0 +1,34 @@
+/*
+ * SequenceLock.hpp
+ *
+ */
+#ifndef INCLUDE_SYSTEM_THREADS_SEQUENCELOCK_HPP_FILE
+#define INCLUDE_SYSTEM_THREADS_SEQUENCELOCK_HPP_FILE
+
+/* public header */
+
+#include <boost/noncopyable.hpp>
+
+namespace System
+{
+namespace Threads
+{
+
+// TODO: move this to separate file
+namespace detail
+{
+} // namespace detail
+
+template<typename TMutex, typename TLock, size_t N>
+class SequenceLock: private boost::noncopyable
+{
+public:
+  // TODO
+  SequenceLock( 
+
+}; // class SequenceLock
+
+} // namespace Threads
+} // namespace System
+
+#endif
