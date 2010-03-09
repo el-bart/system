54928bc3eb494ef7594d15b4159e04cb42b62e5c
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-03-09 09:11:12 +0100

    code has been commented.
    modified:
      system/System/Timer.hpp

--- system/System/Timer.hpp
+++ system/System/Timer.hpp
@@ -12,25 +12,35 @@
 namespace System
 {
 
+/** \brief real-time timer class.
+ *
+ *  class implements timer to meauser real (wall) time elapsed.
+ */
 class Timer
 {
 public:
+  /** \brief start timer.
+   */
   Timer(void)
   {
     restart();
   }
-
+  /** \brief restart timer.
+   */
   void restart(void)
   {
     gettimeofday(&start_, NULL);
   }
-
+  /** \brief returns time elapsed since timer's start.
+   *  \return time since start, measured in seconds (and its fractions).
+   */
   double elapsed(void) const
   {
     struct timeval now;
     gettimeofday(&now, NULL);
     return now.tv_sec-start_.tv_sec + (now.tv_usec-start_.tv_usec)/1000000.0;
   }
+
 private:
   struct timeval start_;
 }; // class Timer
