e900d7aa558d05e3507aa65675b9fd594af5faad
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-08-16 15:12:40 +0200

    reverted to the temporar files from previous version (before tagging).
    added:
      system/System/Math/
      system/System/Math/ArraySumFP.hpp
      system/System/Math/CompareFP.hpp

---
+++ system/System/Math/ArraySumFP.hpp
@@ -0,0 +1,18 @@
+/*
+ * ArraySumFP.hpp
+ *
+ */
+
+// TODO: implement this
+
+//
+// double sum=x[0];
+// double smallBits=0.0;
+// for(int i=1; i<n; ++i)
+// {
+//   double y=x[i]+smallBits;
+//   double temp=sum+y;
+//   smallBits=y-(temp-sum);
+//   sum=temp;
+// }
+//
---
+++ system/System/Math/CompareFP.hpp
@@ -0,0 +1,12 @@
+/*
+ * CompareFP.hpp
+ *
+ */
+
+// TODO: implement this.
+
+//
+// fabs(x-y)/(x*x+y*y+e*e) < n*e
+// n - precission
+// e - std::numeric_limits<double>::epsilon()
+//
