31a9622c74d77f69ac34903ac2ed4c2dce6447f3
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-11-24 10:36:47 +0100

    doxygen fixed.
    modified:
      system/System/GlobalInit.hpp

--- system/System/GlobalInit.hpp
+++ system/System/GlobalInit.hpp
@@ -10,10 +10,6 @@
 #include "System/Threads/SafeInitLocking.hpp"
 #include "System/AtExit.hpp"
 
-// TODO: check this code
-// TODO: write tests
-
-
 namespace System
 {
 
@@ -37,14 +33,54 @@ Threads::SafeInitLock::MutexType &getGlabalInitMutex(void);
  * re-initialized and deallocated stright away.
  *
  * used-defined T type has to provide static methods init() and uninit()
- * that performs as-is initialization (i.e. no locking, checks, etc...)
- * and uninitialization of given element, respecitively.
+ * that performs as-is initialization (i.e. no locking, checks, etc... are
+ * required - it is all done by GlobalInit<> template) and uninitialization
+ * of given element, respecitively.
  * when init() throws, uninit() is never called.  uninit() must NOT throw!
+ *
+ * init() is ensured to be called exactly once, during the first object's
+ * creation (thread safe, with valid object creation order preserved).
+ * uninit() is ensured to be called exactly once (if init() didn't throw)
+ * when program exits. it is ensured that uninit() will NOT be called
+ * until main() exits and last object holds GlobalInit<> copy. notice however
+ * that when new GlobalInit<> instances will be created after leaving main()
+ * it may happen that init()+uninit() sequence will be called more than once,
+ * in proper order.
+ *
+ * typical usage in code goes as follows:
+ * <code>
+ *   namespace detail
+ *   {
+ *     struct MyLibInitImpl
+ *     {
+ *       static void init(void)
+ *       {
+ *         // initialization code for MyLib here
+ *       }
+ *       static void uninit(void)
+ *       {
+ *         // uninitialization code for MyLib here
+ *       }
+ *     };
+ *   }
+ *   typedef System::GlobalInit<detail::MyLibInitImpl> MyLibInit;
+ *   // ...
+ *   MyLibInit mylibinit;
+ *   // from this point in time MyLib is initialized and can be used.
+ *   // ...
+ * </code>
+ *
+ * notice that it is good practice to keep GlobalInit<> instance as a private
+ * object member for those elements that relay on given library to be initialized.
  */
 template<typename T>
 class GlobalInit
 {
 public:
+  /** \brief create handle object.
+   *
+   *  initializes library, if not already initialized. increases usage count.
+   */
   GlobalInit(void)
   {
     Threads::SafeInitLock lock( detail::getGlabalInitMutex() );
@@ -61,6 +97,10 @@ public:
     ++counter(lock);
   }
 
+  /** \brief copy constructor.
+   *
+   *  increases reference count.
+   */
   GlobalInit(const GlobalInit &/*other*/)
   {
     Threads::SafeInitLock lock( detail::getGlabalInitMutex() );
@@ -69,6 +109,9 @@ public:
     ++counter(lock);
   }
 
+  /** \brief assignment operator.
+   *  \return const-reference to this object.
+   */
   const GlobalInit &operator=(const GlobalInit &/*other*/)
   {
     // NOTE: nothing has to be done here, since each copy is equivalent to
@@ -78,6 +121,11 @@ public:
     return *this;
   }
 
+  /** \brief destroy object being referenced.
+   *
+   *  decreases reference count. if this is last instance holding reference
+   *  and atexit() is already done, this will uninitialize library as well.
+   */
   ~GlobalInit(void)
   {
     Threads::SafeInitLock lock( detail::getGlabalInitMutex() );
