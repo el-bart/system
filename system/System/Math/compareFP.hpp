02c4c281697d6dfb80f319453c2e4e77b35cd54b
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-01-18 08:41:33 +0100

    added protection against comparison FP of non-floating-point numbers.
    modified:
      system/System/Math/compareFP.hpp

--- system/System/Math/compareFP.hpp
+++ system/System/Math/compareFP.hpp
@@ -9,6 +9,8 @@
 
 #include <limits>
 #include <cmath>
+#include <boost/static_assert.hpp>
+#include <boost/type_traits/is_floating_point.hpp>
 
 #include "System/Math/ExceptionInvalidPrecision.hpp"
 
@@ -26,7 +28,8 @@ namespace Math
 template<typename FPType>
 bool compareFP(const FPType x, const FPType y, const size_t prec=1)
 {
-  // sanity check
+  // sanity checks
+  BOOST_STATIC_ASSERT( boost::is_floating_point<FPType>::value );
   if(prec<1)
     throw ExceptionInvalidPrecision(SYSTEM_SAVE_LOCATION, "precision must be positive", prec);
   // just to make things faster
