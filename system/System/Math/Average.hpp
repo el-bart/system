e1610f8d8a156ae20297cede2deb57538dcfe411
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-01-18 13:51:56 +0100

    Math::Average has been implemented and tested.
    modified:
      system/System/Math/Average.hpp
      system/System/Math/Average.t.cpp

--- system/System/Math/Average.hpp
+++ system/System/Math/Average.hpp
@@ -5,6 +5,10 @@
 #ifndef INCLUDE_SYSTEM_MATH_AVERAGE_HPP
 #define INCLUDE_SYSTEM_MATH_AVERAGE_HPP
 
+/* public header */
+
+#include <algorithm>
+#include <vector>
 #include <cmath>
 #include <cassert>
 #include <boost/static_assert.hpp>
@@ -17,40 +21,62 @@ namespace System
 namespace Math
 {
 
-template<typename FP, typename TConstIter, FP (*sumFP)(TConstIter, TConstIter)>
+/** \brief computes average and standard deviation from a given sample.
+ *  \param FP floating-point type to represent results and temporary results.
+ */
+template<typename FP>
 class Average
 {
 public:
+  /** \brief compute statistics from a given values.
+   *  \param begin iterator to start reading from.
+   *  \param end   end data iterator.
+   */
+  template<typename TConstIter>
   Average(const TConstIter begin, const TConstIter end):
     count_(0u),
     avg_(0.0),
     stdDev_(0.0)
   {
+    // vector of data will be needed any way later for precise computations
+    std::vector<FP> tmp(begin, end);
+
     // count number of elements
-    for(TConstIter it=begin; it!=end; ++it)
-      ++count_;
+    count_=tmp.size();
 
     // compute the average
-    avg_=(*sumFP)(begin, end)/count_;
+    std::sort( tmp.begin(), tmp.end() );
+    avg_=arraySumFastFP<FP>( tmp.begin(), tmp.end() )/count();
 
-    // compute standard deviation
+    // special case
     if(count_==1u)
     {
       stdDev_=0;
       return;
     }
-    assert(count_>1);
-    // TODO: work in progress
+
+    // compute standard deviation
+    assert( count()>1u );
+    for(typename std::vector<FP>::iterator it=tmp.begin(); it!=tmp.end(); ++it)
+      *it=std::pow(*it-avg(), 2);
+    std::sort( tmp.begin(), tmp.end() );
+    stdDev_=std::sqrt( arraySumFastFP<FP>( tmp.begin(), tmp.end() ) / ( count()-1 ) );
   }
 
+  /** \brief gets number of elements in a given sample.
+   */
   size_t count(void) const
   {
     return count_;
   }
+  /** \brief gets average value on the sample.
+   */
   FP avg(void) const
   {
     return avg_;
   }
+  /** \brief gets standard deviation value on the sample.
+   */
   FP stdDev(void) const
   {
     return stdDev_;
