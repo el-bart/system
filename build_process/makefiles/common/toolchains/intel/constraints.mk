9d31a78b5af5a7869d5f42311b7b9c5489d94408
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-05-11 11:20:05 +0200

    build v2.0.1 introduced.
    modified:
      build_process/makefiles/common/toolchains/intel/constraints.mk

--- build_process/makefiles/common/toolchains/intel/constraints.mk
+++ build_process/makefiles/common/toolchains/intel/constraints.mk
@@ -16,3 +16,24 @@ ifneq (ok,$(shell $(CC) -dumpversion | sed 's:\..*::' | xargs test 12 -le && ech
  $(shell echo "-----------------------------------------------------------------------------------" >&2)
 endif # too old (i.e. buggy) icc version
 endif # with ccache
+
+#
+# work around for GCC 4.5/C++0x libs for intel (required when GCC>=4.5 is default one)
+#
+ifeq ($(TC),intel)
+# BR-hack is required, since inserting brackets directly inside $(shell ) breaks syntax...
+BR:=)
+GCC_VER:=$(shell g++ --version | head -1 | sed "s:^[^$(BR)]\+$(BR) *::" | sed 's: .*::' | sed 's:^\([0-9]\+\.[0-9]\+\).*:\1:')
+BR:=
+ifeq ($(GCC_VER),4.5)
+$(warning WARNING: compiling with ICC, using GCC 4.5 libs - this may lead to problems)
+ifeq ($(shell test -e /usr/include/c++/4.4 && echo OK),OK)
+$(warning WARNING: detected libs for GCC 4.4 - using these ones instead)
+TMP+=-I /usr/include/c++/4.4
+else
+$(warning GCC 4.5 IS (AS FOR NOW) INCOMPATIBLE WITH ICC, SINCE IT USES IMPLICIT C++0x IN)
+$(warning HEADER FILES (INTEL DOES NOT MAKE SUCH AN ASSUMPTION). PROGRAM PROBABLY WILL NOT COMPILE.)
+$(warning TO FIX THIS ISSUE TRY INSTALLING GCC 4.4 OR NEWER ICC.)
+endif # has gcc 4.4 libs
+endif # gcc 4.5
+endif # intel
