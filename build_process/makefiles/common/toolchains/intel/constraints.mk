6857e9e653b722114544594f7337ea0f3cb69f17
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-06-28 16:57:44 +0200

    new build (development version).
    modified:
      build_process/makefiles/common/toolchains/intel/constraints.mk

--- build_process/makefiles/common/toolchains/intel/constraints.mk
+++ build_process/makefiles/common/toolchains/intel/constraints.mk
@@ -1,10 +1,20 @@
+include $(MAKEFILES_TOOLCHAINS_BASE_DIR)/$(TC)/tools.mk
+
+# check version of this toolchain
+INTEL_VERSION:=$(shell $(CC) -dumpversion | sed -e 's:\.: * 10000 + 100 * :' -e 's:\.: + :' | xargs expr)
+
+# check gcc version (icc uses its libs)
+# BR-hack is required, since inserting brackets directly inside $(shell ) breaks syntax...
+BR:=)
+GCC_VER:=$(shell g++ --version | head -1 | sed -e "s:^[^$(BR)]\+$(BR) *::" -e 's: .*::' -e 's:^\([0-9]\+\.[0-9]\+\).*:\1:')
+BR:=
+
 #
 # show warning when using ccache with old intel compiler
 #
 ifneq (,$(WITH_CCACHE))
-include $(MAKEFILES_TOOLCHAINS_BASE_DIR)/$(TC)/tools.mk
 # this warning is true only for ICC releaseses < 12.0.0
-ifneq (ok,$(shell $(CC) -dumpversion | sed 's:\..*::' | xargs test 12 -le && echo 'ok'))
+ifeq (oops,$(shell test $(INTEL_VERSION) -lt 120000 && echo 'oops'))
  $(shell echo "-----------------------------------------------------------------------------------" >&2)
  $(shell echo "                 USING INTEL's TOOLCHAIN WITH CCACHE ENABLED"                        >&2)
  $(shell echo "WARNING: Intel's compiler in version before 12.0.0 is broken - it does not work"     >&2)
@@ -18,20 +28,17 @@ endif # too old (i.e. buggy) icc version
 endif # with ccache
 
 #
-# work around for GCC 4.5/C++0x libs for intel (required when GCC>=4.5 is default one)
+# warn about GCC 4.5/C++0x libs for intel (required when GCC>=4.5 is default one)
 #
-ifeq ($(TC),intel)
-# BR-hack is required, since inserting brackets directly inside $(shell ) breaks syntax...
-BR:=)
-GCC_VER:=$(shell g++ --version | head -1 | sed "s:^[^$(BR)]\+$(BR) *::" | sed 's: .*::' | sed 's:^\([0-9]\+\.[0-9]\+\).*:\1:')
-BR:=
-ifeq ($(GCC_VER),4.5)
+ifeq (oops,$(shell test $(INTEL_VERSION) -lt 120004 && echo 'oops'))
+# this warning is true only for GCC releaseses > 4.4
+ifeq (oops,$(shell echo '$(GCC_VER)' | grep -q '^4\.[56789]' && echo 'oops'))
 $(shell echo "--------------------------------------------------------------------------------------------------" >&2)
-$(shell echo "           COMPILING WITH ICC, USING GCC 4.5 LIBS - THIS MAY LEAD TO PROBLEMS"                      >&2)
+$(shell echo "         COMPILING WITH ICC<12.0.4, USING GCC>=4.5 LIBS - THIS MAY LEAD TO PROBLEMS"                >&2)
 $(shell echo "WARNING: gcc 4.5 is (as for now) incompatible with icc, since it uses implicit c++0x in"            >&2)
 $(shell echo "WARNING: header files (intel does not make such an assumption). program probably will not compile." >&2)
 $(shell echo "WARNING: to fix this issue try installing gcc 4.4 or newer icc."                                    >&2)
 $(shell echo "WARNING: you may want to use gcc 4.4 libs instead (via -I in config.mk)"                            >&2)
 $(shell echo "--------------------------------------------------------------------------------------------------" >&2)
-endif # gcc 4.5
-endif # intel
+endif # gcc>=4.5
+endif # intel<12.0.4
